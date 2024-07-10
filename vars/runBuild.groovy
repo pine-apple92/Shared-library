33eb713ebbfbb9221c4c96642f831ba9583254d8
pine-apple92 <83629539+pine-apple92@users.noreply.github.com>
2024-07-10 20:09:43 +0900

    test
    test

---
+++ vars/runBuild.groovy
@@ -0,0 +1,16 @@
+import groovy.yaml.YamlSlurper
+
+def call(String yamlFilePath = 'jenkins.yml') {
+    // YAML 파일을 읽기
+    def yamlContent = readFile(yamlFilePath)
+    def yaml = new YamlSlurper().parseText(yamlContent)
+
+    // build 섹션의 명령어 실행
+    if (yaml.build) {
+        yaml.build.each { command ->
+            sh command
+        }
+    } else {
+        error "No build section found in ${yamlFilePath}"
+    }
+}
