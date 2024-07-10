27dd4df28b68365381623ae368e532f7a4fdabdf
pine-apple92 <83629539+pine-apple92@users.noreply.github.com>
2024-07-10 20:40:43 +0900

    Update runBuild.groovy

--- vars/runBuild.groovy
+++ vars/runBuild.groovy
@@ -1,9 +1,6 @@
-import groovy.yaml.YamlSlurper
-
 def call(String yamlFilePath = 'jenkins.yml') {
     // YAML 파일을 읽기
-    def yamlContent = readFile(yamlFilePath)
-    def yaml = new YamlSlurper().parseText(yamlContent)
+    def yaml = readYaml file: yamlFilePath
 
     // build 섹션의 명령어 실행
     if (yaml.build) {
