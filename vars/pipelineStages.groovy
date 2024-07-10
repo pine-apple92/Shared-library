942a6153ca56b6a4ccf72710b029c3f872c97a35
pine-apple92 <83629539+pine-apple92@users.noreply.github.com>
2024-07-10 20:21:12 +0900

    Update pipelineStages.groovy

--- vars/pipelineStages.groovy
+++ vars/pipelineStages.groovy
@@ -1,25 +1,28 @@
 def call() {
-    return {
-        stage('Checkout') {
-            steps {
-                checkout scm
-            }
-        }
-
-        stage('Build') {
-            steps {
-                script {
-                    // jenkins.yml 파일에서 빌드 명령어를 읽어와 실행
-                    runBuild()
+    pipeline {
+        agent any
+        stages{
+            stage('Checkout') {
+                steps {
+                    checkout scm
                 }
             }
-        }
 
-        stage('Deploy') {
-            steps {
-                echo 'Deploying...'
-                // 배포 명령어 추가
-                //sh './deploy.sh'
+            stage('Build') {
+                steps {
+                    script {
+                        // jenkins.yml 파일에서 빌드 명령어를 읽어와 실행
+                        runBuild()
+                    }
+                }
+            }
+
+            stage('Deploy') {
+                steps {
+                    echo 'Deploying...'
+                    // 배포 명령어 추가
+                    //sh './deploy.sh'
+                }
             }
         }
     }
